a04efcc834aa1511d20e9b9f07d68425d31a0671
Matthew Defenderfer <mdefende@uab.edu>
2022-04-18 15:12:51 -0500

    create example postFS scripts

---
+++ docs/analysis/hcp_pipelines/scripts/PostFreeSurfer_array_wrapper.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+#SBATCH --job-name=PostFS-%a
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=24:00:00
+#SBATCH --partition=medium
+#SBATCH --output=/home/mdefende/Desktop/hcp-test/code/PostFreeSurfer-%A-%a.txt
+#
+# load necessary modules
+module load FSL/6.0.3
+source ${FSLDIR}/etc/fslconf/fsl.sh
+module load FreeSurfer/6.0.0-centos6_x86_64
+
+# set necessary environmental variables
+export HCPPIPEDIR=$HOME/Scripts/HCPpipelines
+export CARET7DIR=$HOME/Scripts/workbench/bin_rh_linux64
+export HCPPIPEDIR_templates=${HCPPIPEDIR}/global/templates
+export HCPPIPEDIR_config=${HCPPIPEDIR}/global/config
+
+# set data path and grab the subject for the given array job
+data_path=/home/mdefende/Desktop/hcp-test/D01/nifti
+subjects_dir=$data_path/derivatives/hcp-pipelines/FreeSurfer
+
+subj=$(awk "NR==$(($SLURM_ARRAY_TASK_ID)){print;exit}" $data_path/subjlist.txt)
+
+# change directory to the HCPPIPEDIR PostFreeSurfer directory
+cd ${HCPPIPEDIR}/PostFreeSurfer
+
+# run command with following options
+./PostFreeSurferPipeline.sh \
+    --study-folder=${subjects_dir} \
+	--subject=${subj} \
+	--surfatlasdir=${HCPPIPEDIR_templates}/standard_mesh_atlases \
+	--grayordinatesres=2 \
+	--grayordinatesdir=${HCPPIPEDIR_templates}/91282_Greyordinates \
+	--hiresmesh=164 \
+	--lowresmesh=32 \
+	--subcortgraylabels=${HCPPIPEDIR_config}/FreeSurferCorticalLabelTableLut.txt \
+	--freesurferlabels=${HCPPIPEDIR_config}/FreeSurferAllLut.txt \
+	--refmyelinmaps=${HCPPIPEDIR_templates}/standard_mesh_atlases/Conte69.MyelinMap_BC.164k_fs_LR.dscalar.nii
